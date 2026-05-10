6b97a8c3b39c2986cdb410e161100d3f1fa5a6f7
Maria Julia Paes <maria.julia.silva.paes@gmail.com>
2026-05-10 13:38:37 -0300

    Modelo Relacional e estrutura do SQL

---
+++ bancoSistema.sql
@@ -0,0 +1,27 @@
+	CREATE SCHEMA sistema;
+ 
+	CREATE TABLE sistema.usuario (
+
+
+	);
+
+
+	CREATE TABLE sistema.dataset (
+
+
+	);
+
+
+	CREATE TABLE sistema.versao_dataset (
+
+	);
+
+
+	CREATE TABLE sistema.acesso_versao (
+
+	);
+
+
+	CREATE TABLE sistema.feature (
+
+	);
