e81821706997f5fc4b4a0a31819ce8ec1494d3a3
Maria Julia Paes <maria.julia.silva.paes@gmail.com>
2026-05-11 15:05:09 -0300

    ajuste sql

--- bancoSistema.sql
+++ bancoSistema.sql
@@ -12,9 +12,9 @@ CREATE TABLE sistema.usuario (
 
 CREATE TABLE sistema.dataset (
     id SERIAL,
-    nome VARCHAR(200) NOT NULL,
-    descricao VARCHAR(400),
-    fontes VARCHAR(200),
+    nome VARCHAR NOT NULL,
+    descricao VARCHAR,
+    fontes VARCHAR,
     criador_id INT NOT NULL,
     criado_em TIMESTAMP NOT NULL DEFAULT NOW(),
     CONSTRAINT pk_dataset PRIMARY KEY (id),
@@ -40,12 +40,11 @@ ALTER TABLE sistema.versao_dataset
 
 
 CREATE TABLE sistema.acesso_versao (
-	id SERIAL,
 	versao_id int NOT NULL,
 	usuario_id int NOT NULL,
 	tipo_acesso varchar NOT NULL,
 	acessado_em timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT pk_acesso_versao PRIMARY KEY (id),
+	CONSTRAINT pk_acesso_versao PRIMARY KEY (tipo_acesso, versao_id, usuario_id),
 	CONSTRAINT fk_acesso_versao_versao FOREIGN KEY (versao_id) REFERENCES sistema.versao_dataset(id),
 	CONSTRAINT fk_acesso_versao_usuario FOREIGN KEY (usuario_id) REFERENCES sistema.usuario(id),
 	CONSTRAINT ck_tipo_acesso CHECK (tipo_acesso = 'visualizacao' OR tipo_acesso = 'download')
